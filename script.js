54bec0813e578e328b49497cf5d0fc76789eaee6
g7vind <codingacc408@gmail.com>
2023-09-04 22:18:34 +0530

    login

--- script.js
+++ script.js
@@ -7,4 +7,47 @@ const firebaseConfig = {
     appId: "1:685412479236:web:1e5edbc8621e0d9a04e051",
     measurementId: "G-LNG75DN2HK"
   };
-  const app = initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const firestore = firebase.firestore();
+  const signupBtn = document.querySelector('.signupbtn');
+  signupBtn.addEventListener('click', () => {
+    const name = document.querySelector('#name').value;
+    const username = document.querySelector('#username').value;
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+    auth.createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        const uid = user.uid;
+        firestore.collection('users').doc(uid).set({
+          name: name,
+          username: username,
+          email: email,
+        })
+        .then(() => {
+          alert('User data saved to Firestore');
+
+        })
+        .catch((error) => {
+          alert('Error saving user data to Firestore: ', error);
+        });
+      })
+      .catch((error) => {
+        console.error('Error signing up: ', error);
+      });
+  });
+  const loginForm = document.querySelector('.loginbtn');
+  loginForm.addEventListener('click', () => {
+    const email = document.querySelector('#inUsr').value;
+    const password = document.querySelector('#inPass').value;
+    auth.signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        alert('User logged in: ', user);
+        location.href = "signout.html";
+      })
+      .catch((error) => {
+        alert('Error signing in: ', error);
+      });
+  });

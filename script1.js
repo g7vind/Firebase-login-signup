37993c3d9843d5645e2d7b912b4c721c689f1be3
g7vind <codingacc408@gmail.com>
2023-09-05 20:37:14 +0530

    changed

--- script1.js
+++ script1.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCVuYoNcRpyMP5rPE3iDcLTA67xHs0nZ8Y",
-  authDomain: "login-signup-71635.firebaseapp.com",
-  projectId: "login-signup-71635",
-  storageBucket: "login-signup-71635.appspot.com",
-  messagingSenderId: "685412479236",
-  appId: "1:685412479236:web:1e5edbc8621e0d9a04e051",
-  measurementId: "G-LNG75DN2HK"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();

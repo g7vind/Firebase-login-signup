df2801920d9b3c7c8021934f3c2dd7e8d7b340dd
g7vind <codingacc408@gmail.com>
2023-09-05 19:41:36 +0530

    changed

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-import { auth, firestore } from "./firebaseConfig.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyCVuYoNcRpyMP5rPE3iDcLTA67xHs0nZ8Y",
+  authDomain: "login-signup-71635.firebaseapp.com",
+  projectId: "login-signup-71635",
+  storageBucket: "login-signup-71635.appspot.com",
+  messagingSenderId: "685412479236",
+  appId: "1:685412479236:web:1e5edbc8621e0d9a04e051",
+  measurementId: "G-LNG75DN2HK"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 const signupLabel = document.querySelector('.signup label');
 const loginLabel = document.querySelector('.login label');
 const signupForm = document.querySelector('.registration.form');
--- script1.js
+++ script1.js
@@ -1,4 +1,14 @@
-import { auth } from "./firebaseConfig";
+const firebaseConfig = {
+  apiKey: "AIzaSyCVuYoNcRpyMP5rPE3iDcLTA67xHs0nZ8Y",
+  authDomain: "login-signup-71635.firebaseapp.com",
+  projectId: "login-signup-71635",
+  storageBucket: "login-signup-71635.appspot.com",
+  messagingSenderId: "685412479236",
+  appId: "1:685412479236:web:1e5edbc8621e0d9a04e051",
+  measurementId: "G-LNG75DN2HK"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
 const signoutBtn = document.querySelector('#signoutbtn');
 signoutBtn.addEventListener('click', () => {
   auth.signOut()
